0a96f71a2e3d2201b6ed9939124618548cd0a443
DojiStar <angelonunes@gmail.com>
2023-12-17 15:35:11 -0800

    Adding some game stats

--- src/gamestats.py
+++ src/gamestats.py
@@ -1,3 +1,19 @@
 
 class GameStats:
-    enemiesDefeated = 0
+    totalEnemiesDefeated = 0
+    totalDamageTaken = 0
+    totalDamageDone = 0
+    totalRoundsPlayed = 1
+
+    def incrementRound(self):
+        self.totalRoundsPlayed += 1
+    
+    def incrementEnemeiesDefeated(self):
+        self.totalEnemiesDefeated += 1
+    
+    def addDamageTaken(self, damage):
+        self.totalDamageTaken += damage
+    
+    def addDamageDone(self, damage):
+        self.totalDamageDone += damage
+
